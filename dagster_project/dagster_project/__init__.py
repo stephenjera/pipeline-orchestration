5a2f6a2c5d352c9a5eec855966db8595f418cec6
stephenjera <32124927+stephenjera@users.noreply.github.com>
2023-12-15 16:40:37 +0000

    Updated dagster to work with dbt

--- dagster_project/dagster_project/__init__.py
+++ dagster_project/dagster_project/__init__.py
@@ -1,9 +1,16 @@
+import os
+
 from dagster import Definitions, load_assets_from_modules
+from dagster_dbt import DbtCliResource
 
 from . import assets
+from .constants import dbt_project_dir
 
 all_assets = load_assets_from_modules([assets])
 
 defs = Definitions(
     assets=all_assets,
+    resources={
+        "dbt": DbtCliResource(project_dir=os.fspath(dbt_project_dir)),
+    },
 )
--- dagster_project/dagster_project/assets.py
+++ dagster_project/dagster_project/assets.py
@@ -1,5 +1,18 @@
-from dagster import asset
+from pathlib import Path
 
-@asset()
+from dagster import AssetExecutionContext, asset
+from dagster_dbt import DbtCliResource, dbt_assets, get_asset_key_for_model
+
+from .constants import dbt_manifest_path
+
+
+@dbt_assets(manifest=dbt_manifest_path)
+def dbt_project_assets(context: AssetExecutionContext, dbt: DbtCliResource):
+    yield from dbt.cli(["build"], context=context).stream()
+
+
+@asset(
+    deps=[get_asset_key_for_model([dbt_project_assets], "stg_superstore__orders")],
+)
 def hello_world():
     return "Hello World"
