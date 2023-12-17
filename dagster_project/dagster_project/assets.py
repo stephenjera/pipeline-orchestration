0f94812e7159dafdf9bd856b283542a19d982dc2
stephenjera <32124927+stephenjera@users.noreply.github.com>
2023-12-17 23:02:58 +0000

    Created asset to download CSV for Tableau

--- dagster_project/dagster_project/assets.py
+++ dagster_project/dagster_project/assets.py
@@ -1,3 +1,5 @@
+import pandas as pd
+import psycopg2
 from dagster import AssetExecutionContext, asset
 from dagster_airbyte import load_assets_from_airbyte_instance
 from dagster_dbt import DbtCliResource, dbt_assets, get_asset_key_for_model
@@ -19,7 +21,22 @@ def dbt_project_assets(context: AssetExecutionContext, dbt: DbtCliResource):
     deps=[
         get_asset_key_for_model([dbt_project_assets], "global_superstore"),
     ],
-    group_name="Hello",
+    group_name="Tableau",
 )
-def hello_world():
-    return "Hello World"
+def save_to_csv_global_superstore():
+    # Connect to your postgres DB
+    conn = psycopg2.connect(
+        dbname="analytics",
+        user="docker",
+        password="docker",
+        host="localhost",
+        port="5432",
+    )
+
+    # Use a context manager to handle the cursor
+    with conn.cursor() as cur:
+        cur.execute("SELECT * FROM dbt_marts.global_superstore")
+        rows = cur.fetchall()
+        column_names = [desc[0] for desc in cur.description]
+        df = pd.DataFrame(rows, columns=column_names)
+        df.to_csv("./data/global_superstore.csv", index=False)
